0cc21c488942c8ee8f46a62ae105535e8f0d62b2
4lch4 <git@4lch4.email>
2022-05-03 15:59:04 -0500

    feat: fixed return value as string and verify file exists

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { execaCommand } from 'execa'
+import { execaCommand, ExecaReturnValue } from 'execa'
+import { existsSync } from 'fs-extra'
 
 /**
  * Reads a given Dhall file using the dhall-to-json CLI tool, returning the
@@ -7,6 +8,13 @@ import { execaCommand } from 'execa'
  * @param filePath The path to the Dhall file to convert to JSON.
  * @returns A JSON object representing the Dhall file.
  */
-export async function readFile(filePath: string) {
-  return execaCommand(`dhall-to-json --file ${filePath}`)
+export async function readFile(filePath: string): Promise<string | ExecaReturnValue> {
+  if (existsSync(filePath)) {
+    const res = await execaCommand(`dhall-to-json --file ${filePath}`)
+
+    if (res.exitCode === 0) return JSON.parse(res.stdout)
+    else return res
+  } else {
+    throw new Error(`File not found: ${filePath}`)
+  }
 }
